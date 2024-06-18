6deb0305c45da4b3cdef25d98904a8fd4892060e
Erick Peneluca <erickpeneluca@gmail.com>
2024-06-17 23:26:27 -0300

    js-autodata-and-fix-formVal-html

---
+++ script.js
@@ -0,0 +1,26 @@
+const date = new Date();
+
+let ano = date.getFullYear();
+let mes = date.getMonth();
+let dia = date.getDay();
+
+let dataFormatada;
+
+
+if ( dia <= 9 && mes <= 9) {
+    dataFormatada = ano + `-` + `0` + mes + `-`+ `0` + dia;
+    
+}else if (mes <=9) {
+    
+    dataFormatada = ano + `-` + `0` + mes + `-` + dia;
+    
+} else if (dia <= 9){
+    dataFormatada = ano + `-` + mes + `-` + `0` + dia;
+    
+} else if ( dia <= 0 || dia >= 32 || mes <= 0 || mes >= 13 ) {
+    window.print("Erro");
+} else {
+    dataFormatada = ano + `-` + mes + `-` + dia;
+}
+
+window.document.getElementById("data").innerHTML = dataFormatada;
